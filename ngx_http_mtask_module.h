21f98bd70ef6d234ef0d9557d6248c7c7a4cbfbd
Roman Arutyunyan <rarutyunyan@rarutyunyan-laptop.(none)>
2012-01-07 03:18:36 +0400

    added header to make other modules set mtask handlers

---
+++ ngx_http_mtask_module.h
@@ -0,0 +1,23 @@
+#ifndef __NGX_HTTP_MTASK_H__
+#define __NGX_HTTP_MTASK_H__
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+typedef ngx_int_t (*ngx_http_mtask_handler_pt)(ngx_http_request_t*, ngx_chain_t*);
+
+struct ngx_http_mtask_loc_conf_s {
+
+	ngx_http_mtask_handler_pt handler;
+
+	size_t stack_size;
+
+	ngx_msec_t timeout;
+};
+
+typedef struct ngx_http_mtask_loc_conf_s ngx_http_mtask_loc_conf_t;
+
+extern ngx_module_t ngx_http_mtask_module;
+
+#endif /* __NGX_HTTP_MTASK_H__ */
